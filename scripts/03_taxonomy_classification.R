b3671f733dd25b976aa80bf7bd4422db4f5ff481
Jordan Benrezkallah <jordan.benrezkallah@gmail.com>
2025-01-21 16:46:59 +0100

    Update 03_taxonomy_classification.R

--- scripts/03_taxonomy_classification.R
+++ scripts/03_taxonomy_classification.R
@@ -3,7 +3,7 @@ source("./library.R", echo = FALSE)
 
 # Import  ---- 
 df <- fread(paste0("./data/working_directory/02_scientificName_validation_", Sys.Date(), ".csv"), header = TRUE, 
-            sep = ";", dec = ".", strip.white = FALSE, encoding = "UTF-8")
+            sep = ";", dec = ".", strip.white = FALSE, encoding = "Latin-1")
 
 # Data with the taxonomic classification of wild bees
 taxonomy <- fread("./data/wildBeeClassification.csv", header = TRUE, 
